eb949308bc5463af068427e46212d58e912ffa44
kojesung <js95112345@gmail.com>
2025-01-13 02:16:30 +0900

    feat : 누락된 파일 업로드

---
+++ src/app/login/page.tsx
@@ -0,0 +1,13 @@
+import LoginForm from '@/components/login-form';
+
+export default function Login() {
+    return (
+        <div>
+            <div>
+                <h1>볼래말래 로고</h1>
+                <LoginForm></LoginForm>
+                <div></div>
+            </div>
+        </div>
+    );
+}
