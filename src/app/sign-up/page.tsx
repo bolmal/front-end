cff22cd45449a4c2438ca5a727c9443e2b22adbd
kojesung <js95112345@gmail.com>
2025-01-18 16:07:48 +0900

    feat : 회원가입 페이지 UI 3

--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -9,62 +9,62 @@ export default function SignUp() {
                 </div>
                 <div className="w-[500px]">
                     <p className="font-[600] text-[20px]">아이디</p>
-                    <div className="flex flex-row">
-                        <input className="p-3 border rounded-lg w-full" />
+                    <div className="flex flex-row justify-between">
+                        <input className="p-3 border rounded-[20px] h-[64px] w-[350px]" />
                         <Button size="small">중복 확인</Button>
                     </div>
                 </div>
                 <div className="w-[500px]">
                     <p className="font-[600] text-[20px]">비밀번호</p>
                     <div className="flex flex-row">
-                        <input className="p-3 border rounded-lg w-full" />
+                        <input className="p-3 border rounded-[20px] w-full h-[64px]" />
                     </div>
                 </div>
                 <div className="w-[500px]">
                     <p className="font-[600] text-[20px]">비밀번호 확인</p>
                     <div className="flex flex-row">
-                        <input className="p-3 border rounded-lg w-full" />
+                        <input className="p-3 border rounded-[20px] w-full h-[64px]" />
                     </div>
                 </div>
                 <div className="w-[500px]">
                     <p className="font-[600] text-[20px]">이름</p>
                     <div className="flex flex-row">
-                        <input className="p-3 border rounded-lg w-full" />
+                        <input className="p-3 border rounded-[20px] w-full h-[64px]" />
                     </div>
                 </div>
                 <div className="w-[500px]">
                     <p className="font-[600] text-[20px]">아이디</p>
                     <div className="flex flex-row">
-                        <input className="p-3 border rounded-lg w-full" />
+                        <input className="p-3 border rounded-[20px] w-full h-[64px]" />
                     </div>
                 </div>
                 <div className="w-[500px]">
                     <p className="font-[600] text-[20px]">성별</p>
                     <div className="flex flex-row">
-                        <input className="p-3 border rounded-lg w-full" />
+                        <input className="p-3 border rounded-[20px] w-full h-[64px]" />
                     </div>
                 </div>
                 <div className="w-[500px]">
                     <p className="font-[600] text-[20px]">생년월일</p>
                     <div className="flex flex-row">
-                        <input className="p-3 border rounded-lg w-full" />
+                        <input className="p-3 border rounded-[20px] w-full h-[64px]" />
                     </div>
                 </div>
                 <div className="w-[500px]">
                     <p className="font-[600] text-[20px]">이메일</p>
                     <div className="flex flex-row">
-                        <input className="p-3 border rounded-lg w-full" />
+                        <input className="p-3 border rounded-[20px] w-full h-[64px]" />
                     </div>
                 </div>
                 <div className="w-[500px]">
                     <p className="font-[600] text-[20px]">휴대폰 번호</p>
                     <div className="flex flex-row gap-2">
-                        <div className="w-full flex">
-                            <input className="p-3 border rounded-lg w-[110px] h-[64px]" />
-                            <input className="p-3 border rounded-lg w-[110px] h-[64px]" />
-                            <input className="p-3 border rounded-lg w-[110px] h-[64px]" />
+                        <div className="w-full flex justify-between">
+                            <input className="p-3 border rounded-[20px] w-[110px] h-[64px]" />
+                            <input className="p-3 border rounded-[20px] w-[110px] h-[64px]" />
+                            <input className="p-3 border rounded-[20px] w-[110px] h-[64px]" />
+                            <Button size="small">인증하기</Button>
                         </div>
-                        <Button size="small">인증하기</Button>
                     </div>
                 </div>
                 <Button size="large">회원가입 완료</Button>
