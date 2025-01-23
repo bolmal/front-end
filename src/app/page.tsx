13487282ebfc1b23794adae1033a1f1d94fbef27
kojesung <js95112345@gmail.com>
2025-01-24 00:22:26 +0900

    feat : 광고 컴포넌트 레이아웃

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+import Ad from '@/components/ad';
+
 export default function Home() {
-    return <div></div>;
+    return (
+        <div>
+            <div className="bg-bg-default h-[383px] flex py-[20px] px-[120px]">
+                <Ad></Ad>
+            </div>
+        </div>
+    );
 }
---
+++ src/components/ad.tsx
@@ -0,0 +1,3 @@
+export default function Ad() {
+    return <div className="rounded-[20px] bg-amber-300 w-[894px] h-[320px]">gd</div>;
+}
