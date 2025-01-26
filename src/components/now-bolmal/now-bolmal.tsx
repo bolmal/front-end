bc7b582195380f1cc61d2c70dae1f08d39f1bce0
kojesung <js95112345@gmail.com>
2025-01-26 22:57:03 +0900

    feat : Swiper 요소 추가

--- src/components/now-bolmal/now-bolmal.tsx
+++ src/components/now-bolmal/now-bolmal.tsx
@@ -9,9 +9,7 @@ export default function NowBolmal() {
     const name = useStore((state) => state.userInfo.name);
     return (
         <div>
-            <span className="pt-[10px] pb-[15px]">
-                {name === '' ? '지금, 볼래말래?' : `${name}님을 위한 추천 티켓`}
-            </span>
+            <div className="pt-[10px] mb-[15px]">{name === '' ? '지금, 볼래말래?' : `${name}님을 위한 추천 티켓`}</div>
             <div>{isLoggedIn ? <RecommendLogin></RecommendLogin> : <RecommendLogout></RecommendLogout>}</div>
         </div>
     );
--- src/components/now-bolmal/recommend-logout.tsx
+++ src/components/now-bolmal/recommend-logout.tsx
@@ -1,11 +1,52 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
+import test1 from '../../../public/ㅂㄹㅁㄹ.svg';
+import Image from 'next/image';
+import { Autoplay } from 'swiper/modules';
+import 'swiper/css/navigation';
+import { useState } from 'react';
+import { Swiper as SwiperType } from 'swiper';
 
 export default function RecommendLogout() {
+    const [swiper, setSwiper] = useState<SwiperType | null>(null);
+
+    const handlePrev = () => {
+        if (swiper) swiper.slidePrev();
+    };
+
+    const handleNext = () => {
+        if (swiper) swiper.slideNext();
+    };
+    const testObj = [
+        { id: 1, url: test1 },
+        { id: 2, url: test1 },
+        { id: 3, url: test1 },
+        { id: 4, url: test1 },
+        { id: 5, url: test1 },
+        { id: 6, url: test1 },
+        { id: 7, url: test1 },
+        { id: 8, url: test1 },
+        { id: 9, url: test1 },
+        { id: 10, url: test1 },
+    ];
     return (
-        <div>
-            <Swiper>
-                <SwiperSlide></SwiperSlide>
+        <div className="flex">
+            <button className="w-[50px] h-[50px] bg-gray-500 rounded-[50%] z-50" onClick={handlePrev}>
+                ←
+            </button>
+            <Swiper modules={[Autoplay]} autoplay={{ delay: 3000 }} loop={true} slidesPerView={5} onSwiper={setSwiper}>
+                {testObj.map((component) => (
+                    <SwiperSlide key={component.id}>
+                        <Image
+                            className="w-[204px] h-[272px] bg-black rounded-[10px]"
+                            src={component.url}
+                            alt={component.id.toString()}
+                        ></Image>
+                    </SwiperSlide>
+                ))}
             </Swiper>
+            <button className="w-[50px] h-[50px] bg-gray-500 rounded-[50%] z-50" onClick={handleNext}>
+                →
+            </button>
         </div>
     );
 }
