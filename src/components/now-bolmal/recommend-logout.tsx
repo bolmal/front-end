0805185370167c8b7aef3ac8a4f2f698ee6d91a0
kojesung <js95112345@gmail.com>
2025-01-27 04:37:12 +0900

    feat : 공연정보 레이아웃

--- src/components/now-bolmal/recommend-logout.tsx
+++ src/components/now-bolmal/recommend-logout.tsx
@@ -1,7 +1,7 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
 import test1 from '../../../public/ㅂㄹㅁㄹ.svg';
 import Image from 'next/image';
-import { Autoplay, Navigation } from 'swiper/modules';
+import { Autoplay } from 'swiper/modules';
 import 'swiper/css/navigation';
 import { useState } from 'react';
 import { Swiper as SwiperType } from 'swiper';
@@ -32,35 +32,39 @@ export default function RecommendLogout() {
         <div className="relative w-full">
             <div className="flex justify-between">
                 <button
-                    className="absolute left-[-2%] top-1/2 -translate-y-1/2 w-[50px] h-[50px] bg-gray-500 rounded-[50%] z-50"
+                    className="absolute left-[-2%] top-[136px] -translate-y-1/2 w-[50px] h-[50px] bg-gray-500 rounded-[50%] z-50"
                     onClick={handlePrev}
                 >
                     {'<'}
                 </button>
                 <Swiper
-                    modules={[Autoplay, Navigation]}
+                    modules={[Autoplay]}
                     autoplay={{ delay: 3000 }}
                     loop={true}
                     slidesPerView={5}
-                    navigation
                     onSwiper={setSwiper}
                     spaceBetween={50}
                     className="w-full flex justify-between"
                 >
                     {testObj.map((component) => (
                         <SwiperSlide key={component.id} className="w-[20%] bg-bg-pink">
-                            <div className="flex flex-col justify-center">
+                            <div className="flex flex-col items-center">
                                 <Image
                                     className="w-[204px] h-[272px] bg-black rounded-[10px]"
                                     src={component.url}
                                     alt={component.id.toString()}
                                 ></Image>
+                                <div className="w-[204px]">
+                                    <div>1차 티켓 오픈</div>
+                                    <div>2024.01.09(목) 12PM</div>
+                                    <div>2025 JUNIEL CONert..</div>
+                                </div>
                             </div>
                         </SwiperSlide>
                     ))}
                 </Swiper>
                 <button
-                    className="absolute right-[-2%] top-1/2 -translate-y-1/2 w-[50px] h-[50px] bg-gray-500 rounded-[50%] z-50"
+                    className="absolute right-[-2%] top-[136px] -translate-y-1/2 w-[50px] h-[50px] bg-gray-500 rounded-[50%] z-50"
                     onClick={handleNext}
                 >
                     {'>'}
